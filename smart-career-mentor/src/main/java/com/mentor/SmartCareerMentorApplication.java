6533102028a75f80fd2e6e683a68bd902f69a0d1
William Hall <124220997+Willie1416@users.noreply.github.com>
2025-06-14 15:19:20 +0200

    Add files via upload

---
+++ smart-career-mentor/src/main/java/com/mentor/SmartCareerMentorApplication.java
@@ -0,0 +1,13 @@
+package com.mentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCareerMentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCareerMentorApplication.class, args);
+	}
+
+}
---
+++ smart-career-mentor/src/test/java/com/mentor/SmartCareerMentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCareerMentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
